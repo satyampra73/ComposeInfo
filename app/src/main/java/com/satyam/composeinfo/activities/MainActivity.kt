7e48efba4298429851b40032545530aabee02e6d
Satyam Prajapati <satyam7355pra@gmail.com>
2025-01-04 12:39:09 +0530

    Text Field added and working on some logic

--- app/src/main/java/com/satyam/composeinfo/activities/MainActivity.kt
+++ app/src/main/java/com/satyam/composeinfo/activities/MainActivity.kt
@@ -1,8 +1,5 @@
 package com.satyam.composeinfo.activities
 
-import android.R.attr.label
-import android.R.attr.onClick
-import android.R.attr.text
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -13,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -22,10 +20,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat.startActivity
 import com.satyam.composeinfo.activities.ui.theme.GreenLight
 import com.satyam.composeinfo.activities.ui.theme.PurpleGrey40
 
@@ -41,7 +42,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingApp() {
     val context = LocalContext.current
-    var greeting = remember { mutableStateOf("Log In") }
+    var greeting = remember { mutableStateOf("Welcome To Compose Info") }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -60,16 +61,53 @@ fun GreetingApp() {
             Text(
                 text = greeting.value,
                 fontSize = 24.sp,
-                modifier = Modifier.padding(bottom = 20.dp)
+                modifier = Modifier.padding(bottom = 20.dp),
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+            var mobileNo = remember { mutableStateOf("") }
+            var password = remember { mutableStateOf("") }
+
+            TextField(
+                value = mobileNo.value,
+                onValueChange = {
+                        mobileNo.value = it
+                },
+                modifier = Modifier.padding(bottom = 15.dp).fillMaxWidth()
+                    .padding(horizontal = 20.dp),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Phone
+                ),
+                placeholder = { Text("Enter Mobile Number") }
             )
 
 
+            TextField(
+                value = password.value,
+                onValueChange = {
+                    password.value = it
+                },
+                modifier = Modifier.padding(bottom = 15.dp).fillMaxWidth()
+                    .padding(horizontal = 20.dp),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Password
+                ),
+                placeholder = { Text("Enter Password") },
+                visualTransformation = PasswordVisualTransformation()
+            )
 
-            Button(onClick = { greeting.value = "Logging In"
+
+            Button(onClick = {
+//                greeting.value = "Logging In"
                 val intent = Intent(context, SecondActivity::class.java)
+                intent.putExtra("mobileNo", mobileNo.value)
                 context.startActivity(intent)
-            }) {
-                Text("Change Greeting")
+            },
+modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp)
+                ) {
+                Text("LOG IN")
             }
 
 
@@ -77,6 +115,7 @@ fun GreetingApp() {
     }
 
 }
+
 
 @Preview
 @Composable
